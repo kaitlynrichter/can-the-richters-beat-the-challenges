931ad966209732587a84c34a45546ae3068ef7e2
kaitlynrichter <82899324+kaitlynrichter@users.noreply.github.com>
2021-04-28 12:33:30 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,90 @@
+let levels = [img`
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f 4 4 4 4 f e e e e f 4 4 4 4 f 
+    f 4 5 5 4 f e 5 5 e f 4 5 5 4 f 
+    f f 5 5 f f f 5 5 f f 4 5 5 4 f 
+    f 2 2 2 2 f 7 7 7 7 f 3 3 3 3 f 
+    f f 2 2 f f f 7 7 f f f 3 3 f f 
+    f f 2 2 f f f 7 7 f f f 3 3 f f 
+    f f 1 1 f f f 8 8 f f f c c f f 
+    f f 1 1 f f f 8 8 f f f c c f f 
+    f f 1 1 f f f 8 8 f f f c c f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    `]
+scene.setTile(1, img`
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+    3 3 3 3 1 3 3 3 3 3 3 1 3 3 3 3 
+    3 3 3 3 1 3 3 3 3 3 3 1 3 3 3 3 
+    3 3 3 3 1 3 3 3 3 3 3 1 3 3 3 3 
+    3 3 3 3 1 3 3 3 3 3 3 1 3 3 3 3 
+    3 3 3 3 1 3 3 3 3 3 3 1 3 3 3 3 
+    3 3 3 3 1 3 3 3 3 3 3 1 3 3 3 3 
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+    3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+    3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+    3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+    3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+    3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+    `, true)
+scene.setTile(2, img`
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f 2 2 f f f f f f f 
+    f f f f f f f 2 2 f f f f f f f 
+    f f f f f f f 2 2 f f f f f f f 
+    f f f f f f f 2 2 f f f f f f f 
+    f f f f f f f 2 2 f f f f f f f 
+    f f f f f f f 2 2 f f f f f f f 
+    f f f f f f f 2 2 f f f f f f f 
+    f f f f f f f 2 2 f f f f f f f 
+    f f f f f f f 2 2 f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    `, true)
+scene.setTile(7, img`
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f 7 7 7 7 7 f f f f f f 
+    f f f f f f f f f 7 f f f f f f 
+    f f f f f f f f f 7 f f f f f f 
+    f f f f f f f f f 7 f f f f f f 
+    f f f f f 7 7 7 7 7 f f f f f f 
+    f f f f f 7 f f f f f f f f f f 
+    f f f f f 7 f f f f f f f f f f 
+    f f f f f 7 f f f f f f f f f f 
+    f f f f f 7 7 7 7 7 f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    `, true)
+scene.setTile(3, img`
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f 3 3 3 3 3 f f f f f f 
+    f f f f f f f f f 3 f f f f f f 
+    f f f f f f f f f 3 f f f f f f 
+    f f f f f f f f f 3 f f f f f f 
+    f f f f f 3 3 3 3 3 f f f f f f 
+    f f f f f f f f f 3 f f f f f f 
+    f f f f f f f f f 3 f f f f f f 
+    f f f f f f f f f 3 f f f f f f 
+    f f f f f 3 3 3 3 3 f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    `, true)
